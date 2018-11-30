74b4427ff1b6783be0cef3e6ecd5631a9193d7b2
xwp <xwp_fullstack@163.com>
2018-11-30 20:34:12 +0800

    网站小图标设置

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -7,5 +7,12 @@ from info.modules.home import home_blu
 # 使用蓝图来注册路由
 @home_blu.route("/")
 def index():
-
+    # 返回跟路由页面
     return render_template("index.html")
+
+
+# 网站小图标设置favicon.ico
+@home_blu.route("/favicon.ico")
+def favicon():
+    # favicon.ico返回
+    return current_app.send_static_file("news/favicon.ico")
